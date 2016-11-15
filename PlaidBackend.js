6ee74bda25b878acaf85d23f8af87f3d198473b7
FosterA <fosteralexc@gmail.com>
2016-11-15 23:29:46 +0000

    Decided to implement a simple web request to get back a JSON block of transactions from the test environment. Will implement the full library if time permits

--- PlaidBackend.js
+++ PlaidBackend.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var plaid = require('plaid');
+var requestify = require('requestify');
+
 
 var app = express();
 
-var access_token;
+var access_token = "test_bofa";
 
 app.set('port', process.env.PORT || 3000);
 
@@ -12,6 +14,8 @@ var plaidClient = new plaid.Client('test_id',
                                    plaid.environments.tartan);
 
 console.log(plaid.environments);
+
+
 
 app.listen(3000, function () {
   console.log("listening on port 3000");
@@ -24,48 +28,18 @@ app.use(function(req, res, next) {
 });
 
 
-app.post('/upgrade', function(req, res) {
-  plaidClient.upgradeUser(access_token, upgrade_to, options, callback);
-  if(!access_token){
-    //error handling to handle case where there is no set access token
-  }
-});
-
 app.post('/authenticate', function(req, res) {
   console.log("reached node server endpoint");
-  var public_token = req.header('public_token');
-  console.log("public token is: " + public_token);
+  //var public_token = req.header('public_token');
+  //console.log("public token is: " + public_token);
 
+  requestify.get('https://tartan.plaid.com/connect?client_id=test_id&secret=test_secret&access_token=test_bofa').then(function(response) {
+     // Get the response body (JSON parsed - JSON response or jQuery object in case of XML response)
+     var jsonRes = response.getBody();
+     console.log("Mock Transactions returned");
 
-  // Exchange a public_token for a Plaid access_token
-  plaidClient.exchangeToken(public_token, function(err, exchangeTokenRes) {
-    if (err != null) {
-      // Handle error!
-      console.log("error during the exchangeToken.");
-    } else {
-      // This is your Plaid access token - store somewhere persistent
-      // The access_token can be used to make Plaid API calls to
-      // retrieve accounts and transactions
-      access_token = exchangeTokenRes.access_token;
-      console.log("access token is: " + access_token);
-
-      plaidClient.getAuthUser(access_token, function(err, authRes) {
-        if (err != null) {
-          // Handle error!
-          console.log("error during the getAuthUser.");
-        } else {
-          // An array of accounts for this user, containing account
-          // names, balances, and account and routing numbers.
-          var accounts = authRes.accounts;
-
-          // Return account data
-          //res.send("my response back");
-          res.json({accounts: accounts});
-            console.log("Responded with account into");
-        }
-      });
-    }
-  });
-
+     //send the json response
+     res.send(jsonRes);
+ });
 
 });
